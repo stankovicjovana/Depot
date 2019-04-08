e1c83a28460275a6216df6ccb011a6840bc9e417
jovana <stakovicmjovana@gmail.com>
2019-04-08 10:16:17 +0200

    Generated migration combine_line_items_in_cart with two method, up and down so we can safely rollback migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_07_141647) do
+ActiveRecord::Schema.define(version: 2019_04_08_075619) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false

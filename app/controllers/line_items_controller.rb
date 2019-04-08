d105cb10b8eafad592d87a9dd658eff726cd8856
jovana <stakovicmjovana@gmail.com>
2019-04-08 15:41:13 +0200

    In cart show view added button_to delete one line_item from current cart. Changed destroy method in LineItemsContoller to destroy one by one item.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -59,9 +59,16 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
-    @line_item.destroy
+    @cart = @line_item.cart
+
+    if @line_item.quantity > 1
+      @line_item.quantity = @line_item.quantity - 1
+      @line_item.save
+    else
+      @line_item.destroy
+    end
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to @cart, notice: 'One Line item was successfully removed.' }
       format.json { head :no_content }
     end
   end

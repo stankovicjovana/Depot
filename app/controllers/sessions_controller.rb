57bda57c4253aac3de89fd56f40116ce442992a0
jovana <stakovicmjovana@gmail.com>
2019-04-11 17:41:40 +0200

    Fixed created action in the seesion controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(name: params[:name])
   	if user.try(:authenticate, params[:password])
-  		session[:user_id] = user.user_id
+  		session[:user_id] = user.id
   		redirect_to admin_url
   	else
   		redirect_to login_url, alert: "Invalid user/password combination"

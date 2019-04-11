ae9187b47b430157e3a87cb086b1df378ccb239b
jovana <stakovicmjovana@gmail.com>
2019-04-11 18:00:23 +0200

    Created tests in for admin cntroller: "should get index" for sessions controller: "should prompt for login", "should log in", "should fails to login", and "should logout".

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -16,4 +16,26 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should prompt for login" do
+    get login_url
+    assert_response :success
+  end
+
+  test "should log in" do
+    dave = users(:one)
+    post login_url, params: { name: dave.name, password: 'secret' }
+    assert_redirected_to admin_url
+    assert_equal dave.id, session[:user_id]
+  end
+
+  test "should fails to login" do
+    dave = users(:one)
+    post login_url, params: { name: dave.name, password: 'wrong'}
+    assert_redirected_to login_url
+  end
+
+  test "should logout" do
+    delete logout_url
+    assert_redirected_to store_index_url
+  end
 end

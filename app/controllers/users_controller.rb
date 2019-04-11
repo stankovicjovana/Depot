af036bbbd92cdd3002923c76d4189cfda179fac7
jovana <stakovicmjovana@gmail.com>
2019-04-11 16:35:05 +0200

    Fixed users create action. Added <div class="depot_form"> and ids to form fields.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.order(:name)
   end
 
   # GET /users/1
@@ -28,7 +28,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to users_url, notice: 'User #{@user.name} was successfully created.' }
+        format.html { redirect_to users_url,
+              notice: "User #{@user.name} was successfully updated." }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }

8cf5475795665850ad577bb5dbea7176f1d94b34
jovana <stakovicmjovana@gmail.com>
2019-04-11 15:47:57 +0200

    Written a system test to cover the entire end-to-end workflow, including verifying that the background job executed and the email was sent - "check routing number". we have  4 test errors running orders_test.rb

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	include ActiveJob::TestHelper
+	
+	test "check routing number" do
+		LineItem.delete_all
+		Order.delete_all
+
+		visit store_index_url
+		assert_selector "#order_routing_number"
+
+		fill_in "Routing #", with: "123456"
+		fill_in "Account #", with: "987654"
+
+		#perform any jobs that get enqueued inside the block
+		perform_enqueued_jobs do
+			click_button "Place Order"
+		end
+
+		#check that an order was created in the way we expect
+		orders = Order.all
+		assert_equal 1, orders.size
+
+		order = orders.first
+
+		assert_equal "Dave Thomas", order.name
+		assert_equal "123 Main Street", order.address
+		assert_equal "dave@example.com", order.email
+		assert_equal "Check", order.pay_type
+		assert_equal 1, order.line_items.size
+		
+		#check that the mail was sent
+		mail = ActionMailer::Base.deliveries.last
+		assert_equal ["dave@example.com"], mail.to
+		assert_equal 'Sam Ruby <depot@example.com>', mail[:from].value
+		assert_equal "Pragmatic Store Order Confirmation", mail.subject
+	end
 end
